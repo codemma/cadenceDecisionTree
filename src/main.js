b304b43d289a4f77e7753db51319a4e5c2fe46e3
Emma Tysk <etysk@uber.com>
2020-08-11 15:25:21 +0200

    adding custom css to pop up

--- src/main.js
+++ src/main.js
@@ -67,39 +67,64 @@ var svg = d3.select("svg"),
 var render = new dagreD3.render();
 
 // Set up an SVG group so that we can translate the final graph.
-var svg = d3.select("svg"),
-  zoom = d3.zoom().on("zoom", function () {
-    inner.attr("transform", d3.event.transform);
-    inner.attr("transform", d3.event.transform);
-  });
+var zoom = d3.zoom().on("zoom", function () {
+  inner.attr("transform", d3.event.transform);
+  inner.attr("transform", d3.event.transform);
+});
 svg.call(zoom);
 
 // Run the renderer. This is what draws the final graph.
 render(inner, g);
 
-var tooltip = d3.select("body")
-  .append("div")
-  .style("position", "absolute")
-  .style("background-color", "white")
-  .style("border", "solid")
-  .style("border-width", "2px")
-  .style("border-radius", "5px")
-  .style("padding", "5px")
-  .style("z-index", "10")
-  .style("visibility", "hidden")
-  .text("Simple Tooltip...");
+// Define the div for the tooltip
+/* var tooltip = d3.select("body")
+  .append("div").attr("class", "tooltip") */
 
-//Select all nodes and add click event, also trying out mouseover and mouseout
+/* d3.select("body")
+  .append("div")
+  .attr('id', 'tooltip')
+  .attr('style', 'position: absolute; opacity: 0;'); */
+/*  .style("background-color", "white")
+ .style("border", "solid")
+ .style("border-width", "2px")
+ .style("border-radius", "5px")
+ .style("padding", "5px")
+ .style("z-index", "10")
+ .style("visibility", "hidden")
+ .text("Simple Tooltip..."); */
+
+function tooltipText(d) {
+  return 'Color base: ' + Math.round(d[2] * 100) / 100 + ', Position: ' + Math.round(d[0] * 100) / 100 + ';' + Math.round(d[1] * 100) / 100;
+}
+
+//Select all nodes and add click event
+//ALso trying out mouseover and mouseout
 inner.selectAll('g.node')
   .attr("data-hovertext", function (v) {
     return g.node(v).hovertext
   })
-  .on("click", function () { return tooltip.style("visibility", "visible"); })
-  .on("mousemove", function () {
+  /* .on("click", function () {
     tooltip.text(this.dataset.hovertext)
       .style("top", (event.pageY - 10) + "px")
       .style("left", (event.pageX + 10) + "px");
+    return tooltip.style("visibility", "visible");
+  }) */
+  /* .on('mousemove', function () {
+    d3.select('#tooltip')
+      .style('left', d3.event.pageX + 'px')
+      .style('top', d3.event.pageY + 'px')
+  }) */
+  .on("click", function () {
+    d3.select("#tooltip").classed("hidden", false);
   })
-  .on("mouseout", function () { return tooltip.style("visibility", "hidden"); });
+  .on('mousemove', function (d) {
+    let coordinates = d3.mouse(this);
+    d3.select("#tooltip")
+      .style("left", (event.pageX - 10) + "px")
+      .style("top", (event.pageY + 10) + "px")
+      .select("#info")
+      .text(this.dataset.hovertext);
+  })
+  //.on("mouseout", function () { return tooltip.style("visibility", "hidden"); });
 
 //svg.attr("height", g.graph().height + 50);
