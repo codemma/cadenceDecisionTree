eaf2e0f4e28b8dc1a9455a0d9a81d628cacb030e
Emma Tysk <etysk@uber.com>
2020-09-04 13:22:38 +0300

    added new workflows and routes to them

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,6 +14,30 @@
       <router-link
         :to="{ name: 'tree', params: { runId: '3432067e-fed0-4c4d-9a16-a3395e1eb1d8' }}"
       >HelloActivityRetry</router-link>
+      <router-link
+        :to="{ name: 'tree', params: { runId: '6b03969b-06fa-4325-9c07-9b6b038ccde5' }}"
+      >HelloAsync</router-link>
+      <router-link
+        :to="{ name: 'tree', params: { runId: '18546f28-b06f-4fd2-a5b8-dfeeca5dd195' }}"
+      >HelloAsyncActivityCompletion</router-link>
+      <router-link
+        :to="{ name: 'tree', params: { runId: '4502a990-339f-4d58-b193-b672b2b79f11' }}"
+      >HelloAsyncLambda</router-link>
+      <router-link
+        :to="{ name: 'tree', params: { runId: '4502a990-339f-4d58-b193-b672b2b79f11' }}"
+      >HelloAsyncLambda</router-link>
+      <router-link
+        :to="{ name: 'tree', params: { runId: '1f229f6c-aa12-44e7-8846-a62878691977' }}"
+      >HelloException</router-link>
+      <router-link
+        :to="{ name: 'tree', params: { runId: '657b7334-6f4d-4ad7-ba1d-517178b57963' }}"
+      >HelloPeriodic</router-link>
+      <router-link
+        :to="{ name: 'tree', params: { runId: 'd4f2f2f2-5db7-4fae-aa77-46ab93bc44ca' }}"
+      >HelloQuery</router-link>
+      <router-link
+        :to="{ name: 'tree', params: { runId: '05538fc1-7c7e-4a0b-99a9-3e86e6feca03' }}"
+      >HelloSignal</router-link>
     </div>
   </div>
 </template>
