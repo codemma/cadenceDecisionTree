3a3c7cc791366add5280651cacae9256816fab1c
Emma Tysk <etysk@uber.com>
2020-09-04 11:21:58 +0300

    added a new home component with the routes to the workfows

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="home">
+    <div id="nav">
+      <router-link
+        class="router-link"
+        :to="{ name: 'tree', params: { runId: '957bad90-9c99-4afb-8ccc-0e550a439a60' }}"
+      >Parent workflow</router-link>
+      <router-link
+        :to="{ name: 'tree', params: { runId: 'afc9e3d9-cba6-4df1-b212-919c34addf5b' }}"
+      >Failed workflow</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import router from "../router";
+export default {
+  name: "home",
+};
+</script>
+<!-- styling for the component -->
+<style>
+#about {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#nav {
+  display: flex;
+  flex-direction: column;
+}
+</style>
