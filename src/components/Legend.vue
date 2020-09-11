77755a809e76f7243996476a104e3ec2be236cf3
Emma Tysk <etysk@uber.com>
2020-09-11 17:06:19 +0300

    added simple ledger component

---
+++ src/components/Legend.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="graph-legend">
+    <div>legend</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    runId: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      workflow: {},
+      graph: {},
+      parentArray: [],
+      parentRunId: null,
+      btnText: null,
+      routeId: null,
+      clickedId: null,
+      workflowName: null,
+    };
+  },
+  watch: {},
+  mounted() {},
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style lang="stylus"></style>
