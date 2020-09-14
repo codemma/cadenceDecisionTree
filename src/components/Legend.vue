0d92e4b6bb180edc90bf69d5b1678580bca5e019
Emma Tysk <etysk@uber.com>
2020-09-14 12:44:07 +0300

    added styling to ledger

--- src/components/Legend.vue
+++ src/components/Legend.vue
@@ -1,8 +1,36 @@
 <template>
-  <div class="graph-legend">
-    <!--  <button @click="toggleAllCollapse">Expand/Collapse</button> -->
-    <span class="pic arrow-left"></span>
-    <hr />
+  <div v-on:click="showLegend =!showLegend" class="legend">
+    <div v-if="!showLegend" class="legend-example">
+      <div class="arrow-container">
+        <span class="pic arrow chron"></span>
+        <hr class="chron" />
+      </div>
+    </div>
+    <div v-if="showLegend" class="legend-content">
+      <div class="legend-example">
+        <div class="arrow-container">
+          <span class="pic arrow chron"></span>
+          <hr class="chron" />
+        </div>
+        <div class="text">Represents chronological connections</div>
+      </div>
+      <hr />
+      <div class="legend-example">
+        <div class="arrow-container">
+          <span class="pic arrow direct"></span>
+          <hr class="direct" />
+        </div>
+        <div class="text">Represents chronological connections</div>
+      </div>
+      <hr />
+      <div class="legend-example">
+        <div class="arrow-container">
+          <span class="pic arrow inferred"></span>
+          <hr class="inferred" />
+        </div>
+        <div class="text">Represents chronological connections</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,7 +38,9 @@
 export default {
   props: {},
   data() {
-    return {};
+    return {
+      showLegend: false,
+    };
   },
   watch: {},
   mounted() {},
@@ -20,15 +50,62 @@ export default {
 </script>
 
 <style  scoped lang="stylus">
-.graph-legend {
+.legend {
+  background-color: white;
+  border: 1px solid #eaeaea;
+  box-shadow: 0px 0px 9px 0px rgba(232, 232, 232, 1);
   display: flex;
   justify-content: center;
   align-items: center;
-  box-shadow: 0px 0px 9px 0px rgba(232, 232, 232, 1);
-  width: 100px;
+  flex-direction: column;
+  width: fit-content;
   position: absolute;
   right: 20px;
   top: 80px;
+
+  &-example {
+    display: flex;
+    margin: 20px;
+    flex-direction: column;
+    text-align: left;
+
+    hr {
+      width: 60px;
+      margin-left: 4px;
+
+      &.direct {
+        border-top: 2px solid black;
+      }
+
+      &.chron {
+        border-top: 2px dashed #5879DA;
+      }
+
+      &.inferred {
+        border-top: 2px dashed #ECAB20;
+      }
+    }
+  }
+
+  &-content {
+    > hr {
+      border: 0;
+      margin: 0;
+      border-top: 1px solid #eaeaea;
+      width: 100%;
+      padding: 0;
+    }
+  }
+}
+
+.text {
+  margin-top: 10px;
+  max-width: 300px;
+}
+
+.break {
+  flex-basis: 100%;
+  height: 0;
 }
 
 .pic {
@@ -38,15 +115,29 @@ export default {
   height: 0;
 }
 
-.arrow-left {
-  border-top: 6px solid transparent;
-  border-bottom: 6px solid transparent;
-  border-right: 6px solid #5879DA;
-  margin-left: 20px;
+.arrow-container {
+  display: flex;
+  align-items: center;
 }
 
-hr {
-  border-top: 2px dashed #5879DA;
-  margin: 20px 20px 20px 2px;
+.arrow {
+  border-top: 6px solid transparent;
+  border-bottom: 6px solid transparent;
+
+  &.direct {
+    border-right: 6px solid black;
+  }
+
+  &.chron {
+    border-right: 6px solid #5879DA;
+  }
+
+  &.inferred {
+    border-right: 6px solid #ECAB20;
+  }
+}
+
+ul {
+  list-style-position: inside;
 }
 </style>
