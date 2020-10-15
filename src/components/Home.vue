d8840dd673eb88ac6f2d049d5ed00256baaca72b
Emma Tysk <etysk@uber.com>
2020-10-15 13:07:03 +0300

    removed old workflows and styled demo page a little

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,82 +1,129 @@
 <template>
   <div id="home">
+    <h2>Demo routes</h2>
     <div id="nav">
       <router-link
         class="router-link"
-        :to="{ name: 'tree', params: { runId: '10k-workflow' }}"
-      >10k workflow</router-link>
-      <router-link
-        class="router-link"
-        :to="{ name: 'tree', params: { runId: '5k-workflow' }}"
-      >5k workflow</router-link>
-      <router-link
-        class="router-link"
-        :to="{ name: 'tree', params: { runId: '957bad90-9c99-4afb-8ccc-0e550a439a60' }}"
-      >HelloChild</router-link>
+        :to="{
+          name: 'graphcontainer',
+          params: { runId: '957bad90-9c99-4afb-8ccc-0e550a439a60' },
+        }"
+        >HelloChild</router-link
+      >
       <div class="explanation">workflow with child</div>
       <router-link
         class="router-link"
-        :to="{ name: 'tree', params: { runId: '524629a5-f63e-47b0-a7b1-5e74304d9da3' }}"
-      >HelloActivity</router-link>
+        :to="{
+          name: 'graphcontainer',
+          params: { runId: '524629a5-f63e-47b0-a7b1-5e74304d9da3' },
+        }"
+        >HelloActivity</router-link
+      >
       <div class="explanation">a single activity workflow</div>
       <router-link
         class="router-link"
-        :to="{ name: 'tree', params: { runId: '3432067e-fed0-4c4d-9a16-a3395e1eb1d8' }}"
-      >HelloActivityRetry</router-link>
+        :to="{
+          name: 'graphcontainer',
+          params: { runId: '3432067e-fed0-4c4d-9a16-a3395e1eb1d8' },
+        }"
+        >HelloActivityRetry</router-link
+      >
       <div class="explanation">retrying an activity</div>
       <router-link
         class="router-link"
-        :to="{ name: 'tree', params: { runId: '6b03969b-06fa-4325-9c07-9b6b038ccde5' }}"
-      >HelloAsync</router-link>
-      <div
-        class="explanation"
-      >how to call activities asynchronously and wait for them using Promises</div>
+        :to="{
+          name: 'graphcontainer',
+          params: { runId: '6b03969b-06fa-4325-9c07-9b6b038ccde5' },
+        }"
+        >HelloAsync</router-link
+      >
+      <div class="explanation">
+        how to call activities asynchronously and wait for them using Promises
+      </div>
       <router-link
         class="router-link"
-        :to="{ name: 'tree', params: { runId: '18546f28-b06f-4fd2-a5b8-dfeeca5dd195' }}"
-      >HelloAsyncActivityCompletion</router-link>
+        :to="{
+          name: 'graphcontainer',
+          params: { runId: '18546f28-b06f-4fd2-a5b8-dfeeca5dd195' },
+        }"
+        >HelloAsyncActivityCompletion</router-link
+      >
       <div class="explanation">an asynchronous activity implementation</div>
       <router-link
         class="router-link"
-        :to="{ name: 'tree', params: { runId: '4502a990-339f-4d58-b193-b672b2b79f11' }}"
-      >HelloAsyncLambda</router-link>
-      <div
-        class="explanation"
-      >how to run part of a workflow asynchronously in a separate task (thread)</div>
+        :to="{
+          name: 'graphcontainer',
+          params: { runId: '4502a990-339f-4d58-b193-b672b2b79f11' },
+        }"
+        >HelloAsyncLambda</router-link
+      >
+      <div class="explanation">
+        how to run part of a workflow asynchronously in a separate task (thread)
+      </div>
       <router-link
         class="router-link"
-        :to="{ name: 'tree', params: {  runId: '1f229f6c-aa12-44e7-8846-a62878691977' }}"
-      >HelloException</router-link>
+        :to="{
+          name: 'graphcontainer',
+          params: { runId: '1f229f6c-aa12-44e7-8846-a62878691977' },
+        }"
+        >HelloException</router-link
+      >
       <div class="explanation">exception propagation and wrapping</div>
       <router-link
         class="router-link"
-        :to="{ name: 'tree', params: { runId: '657b7334-6f4d-4ad7-ba1d-517178b57963' }}"
-      >HelloPeriodic</router-link>
-      <div class="explanation">a sample workflow that executes an activity periodically forever</div>
+        :to="{
+          name: 'graphcontainer',
+          params: { runId: '657b7334-6f4d-4ad7-ba1d-517178b57963' },
+        }"
+        >HelloPeriodic</router-link
+      >
+      <div class="explanation">
+        a sample workflow that executes an activity periodically forever
+      </div>
       <router-link
         class="router-link"
-        :to="{ name: 'tree', params: { runId: 'd4f2f2f2-5db7-4fae-aa77-46ab93bc44ca' }}"
-      >HelloQuery</router-link>
+        :to="{
+          name: 'graphcontainer',
+          params: { runId: 'd4f2f2f2-5db7-4fae-aa77-46ab93bc44ca' },
+        }"
+        >HelloQuery</router-link
+      >
       <div class="explanation">a query</div>
       <router-link
         class="router-link"
-        :to="{ name: 'tree', params: { runId: '05538fc1-7c7e-4a0b-99a9-3e86e6feca03' }}"
-      >HelloSignal</router-link>
+        :to="{
+          name: 'graphcontainer',
+          params: { runId: '05538fc1-7c7e-4a0b-99a9-3e86e6feca03' },
+        }"
+        >HelloSignal</router-link
+      >
       <div class="explanation">sending and handling a signal</div>
       <router-link
         class="router-link"
-        :to="{ name: 'tree', params: { runId: 'fe80f571-639d-471b-b7a0-2fd49bc052cf' }}"
-      >HelloSearchAttributes</router-link>
+        :to="{
+          name: 'graphcontainer',
+          params: { runId: 'fe80f571-639d-471b-b7a0-2fd49bc052cf' },
+        }"
+        >HelloSearchAttributes</router-link
+      >
       <div class="explanation">how to use search attributes</div>
       <router-link
         class="router-link"
-        :to="{ name: 'tree', params: { runId: 'a57bb2c9-1f78-4cf8-b0b4-063932953deb' }}"
-      >HelloCron</router-link>
+        :to="{
+          name: 'graphcontainer',
+          params: { runId: 'a57bb2c9-1f78-4cf8-b0b4-063932953deb' },
+        }"
+        >HelloCron</router-link
+      >
       <div class="explanation">a cron workflow</div>
       <router-link
         class="router-link"
-        :to="{ name: 'tree', params: { runId: 'signal-and-children' }}"
-      >Large workflow</router-link>
+        :to="{
+          name: 'graphcontainer',
+          params: { runId: 'signal-and-children' },
+        }"
+        >Large workflow</router-link
+      >
       <div class="explanation">A larger workflow with signals</div>
     </div>
   </div>
@@ -96,6 +143,10 @@ export default {
   overflow-y: scroll;
   height: 100%;
   width: 100%;
+
+  h2 {
+    margin: 1em 0;
+  }
 }
 
 .explanation {
